1b4aabec5e01eb2ec9a6fbc3614bf224585e5ffa
mehmetcanozturk <kaptanhiro123@gmail.com>
2022-07-13 20:06:18 +0300

    navbar, slider and carousel edited

--- src/views/Content/Content.jsx
+++ src/views/Content/Content.jsx
@@ -10,7 +10,10 @@ function Content() {
     return (
         <>
             <SliderShow category={MovieDb.Categories.NOW_PLAYING}/>
-            <SliderCarousel title="Sinemalarda" category={MovieDb.Categories.NOW_PLAYING} />
+            <SliderCarousel title="Popüler Filmler" category={MovieDb.Categories.POPULAR} />
+            <SliderCarousel title="En Çok Oy Alan Filmler" category={MovieDb.Categories.TOP_RATED} />
+            <SliderCarousel title="Vizyondaki Filmler" category={MovieDb.Categories.NOW_PLAYING} />
+            <SliderCarousel title="Vizyona Girecek Filmler" category={MovieDb.Categories.UPCOMING} />
         </>
     )
 }
--- src/views/navbar/Navbar.jsx
+++ src/views/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import './Navbar.scss'
 function Navbar() {
     return (
         <div className="topnav">
-            <Link to='/' className="active">ReactFlix</Link>
+            <Link to='/' className="active">Film Website</Link>
             <Link to="/favorites">Favorites</Link>
             <Link to='/contact'>Contact</Link>
         </div>
