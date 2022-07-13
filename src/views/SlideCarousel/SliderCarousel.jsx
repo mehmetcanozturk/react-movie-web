e0bab7b5cdb1e93d50743707a06cf75e4525242d
mehmetcanozturk <kaptanhiro123@gmail.com>
2022-07-13 14:13:25 +0300

    carousel fixed

--- src/views/SlideCarousel/SliderCarousel.jsx
+++ src/views/SlideCarousel/SliderCarousel.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
+import React from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 import Card from '../../components/Card/Card'
-import './SliderCarousel.scss'
+
+// Import Swiper styles
+import "swiper/css";
+
+import "./SliderCarousel.scss";
 
 function SliderCarousel(props) {
   return (
-    <div className='carousel-container'>
-        <div className='title'>
+    <div className="carousel-container">
+      <h3>
             {props.title}
-        </div>
-        <div className='cards'>
-            {
+      </h3>
+      <div className='carousel-cards'>      <div className='cards'>
+      <Swiper watchSlidesProgress={true} slidesPerView={7} className="mySwiper">
+      {
                 props.list.map((data)=>{
-                   return <Card data={data}/>
+                   return <SwiperSlide><Card data={data}/></SwiperSlide>
                 })
             }
-        </div>
-    </div>
-  )
-}
 
+      </Swiper>
+      </div>
+      </div>
+
+
+    </div>
+  );
+}
 export default SliderCarousel
